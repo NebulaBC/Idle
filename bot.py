f6c9cafd6335b957c7dc77e045893686ec111d81
CheesyBC <thecheeseburgerfactory32@gmail.com>
2021-02-03 23:37:28 -0500

    commit

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ from discord.ext.commands import Bot
 intents = discord.Intents.default()
 intents.members = True
 
-bot = Bot(">", intents=intents)
+bot = Bot(">", intents=intents, case_insensitive=True)
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -26,7 +27,13 @@ async def on_member_remove(member):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('Pong')
+    await ctx.send(f'Ping: {round(bot.latency * 1000)}ms')
+
+@bot.command()
+async def help(ctx):
+    embedVar = discord.Embed(title="Help", description="", color=0x3410d4)
+    embedVar.add_field(name=">ping", value="Get the ping of IDLE bot", inline=False)
+    await ctx.send(embed=embedVar)
 
 bot.run('')
 #client.run(os.getenv('IDLE_BOT_TOKEN'))
