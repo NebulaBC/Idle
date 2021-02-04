89b60cfb703f9d373354586bc75a541101fda462
CheesyBC <thecheeseburgerfactory32@gmail.com>
2021-02-04 09:07:02 -0500

    Added Kick and Ban

--- bot.py
+++ bot.py
@@ -14,16 +14,16 @@ bot.remove_command('help')
 @bot.event
 async def on_ready():
     print(f'{bot.user} is ready.')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= str(len(bot.guilds)) + " servers"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= str(len(bot.guilds)) + " servers | >help"))
 
 @bot.event
 async def on_member_join(member):
-    await member.send('Welcome to the server!')
-    print (f'{member} has joined the server')
+    await member.send(f'Welcome to {member.guild.name}!')
+    print (f'{member} has joined a server')
 
 @bot.event
 async def on_member_remove(member):
-    print (f'{member} has left the server')
+    print (f'{member} has left a server')
 
 @bot.command()
 async def ping(ctx):
@@ -33,7 +33,23 @@ async def ping(ctx):
 async def help(ctx):
     embedVar = discord.Embed(title="Help", description="", color=0x3410d4)
     embedVar.add_field(name=">ping", value="Get the ping of IDLE bot", inline=False)
+    embedVar.add_field(name=">kick <@member> <Reason>", value="Kick a member from the discord server", inline=False)
+    embedVar.add_field(name=">ban <@member> <Reason>", value="Ban a member from the discord server", inline=False)
     await ctx.send(embed=embedVar)
 
-bot.run('Token here or use .env')
+@bot.command()
+async def clear(ctx, amount=6):
+    await ctx.channel.purge(limit=amount + 1)
+
+@bot.command()
+async def kick(ctx, user: discord.Member):
+    await ctx.send(f'{user.name} Has Been Kicked!')
+    await ctx.guild.kick(user)
+
+@bot.command()
+async def ban(ctx, user: discord.Member):
+    await ctx.send(f'{user.name} Has Been Banished!')
+    await ctx.guild.ban(user)
+
+bot.run('')
 #client.run(os.getenv('IDLE_BOT_TOKEN'))
