c83462471f8af69ba3845a0ad11d85bcfd9aff19
CheesyBC <thecheeseburgerfactory32@gmail.com>
2021-02-03 23:19:20 -0500

    commit

--- bot.py
+++ bot.py
@@ -1,27 +1,32 @@
 import discord
-import os
+from discord.ext.commands import Bot
+#from dotenv import load_dotenv
+#import os
+
+#load_dotenv(.env)
 
 intents = discord.Intents.default()
 intents.members = True
 
-client = discord.Client(command_prefix='>', intents=intents)
+bot = Bot(">", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} is ready.')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= str(len(client.guilds)) + " servers"))
+    print(f'{bot.user} is ready.')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= str(len(bot.guilds)) + " servers"))
 
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.send('Welcome to the server!')
     print (f'{member} has joined the server')
 
-@client.event
+@bot.event
 async def on_member_remove(member):
     print (f'{member} has left the server')
 
-@client.command()
+@bot.command()
 async def ping(ctx):
     await ctx.send('Pong')
 
-client.run('')
+bot.run('')
+#client.run(os.getenv('IDLE_BOT_TOKEN'))
